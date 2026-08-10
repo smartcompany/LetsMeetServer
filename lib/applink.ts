de147cd658cd4eae4ec29ec306b3aae7a23f251c
yong geon <gunnylove@gmail.com>
2026-08-10 15:20:16 +0900

    update

--- lib/applink.ts
+++ lib/applink.ts
@@ -1,10 +1,10 @@
 export const APP_DISPLAY_NAME = '이음터';
 
 export const IOS_APP_STORE_WEB =
-  'https://apps.apple.com/app/id6757979087';
+  'https://apps.apple.com/kr/app/id6757979087';
 
 export const PLAY_STORE_WEB =
-  'https://play.google.com/store/apps/details?id=com.smartcompany.letsMeet';
+  'https://play.google.com/store/apps/details?id=com.smartcompany.letsMeet&hl=ko&gl=KR';
 
 export function pickStoreUrl(userAgent: string): string {
   const ua = userAgent.toLowerCase();
